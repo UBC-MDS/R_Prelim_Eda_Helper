6757065bce70f6fed8888c723fba18ec8291f372
meh2307 <mehwishbhat.ubc@gmail.com>
2023-01-22 01:36:33 -0800

    fixed errors in function name and body

--- R/num_dist_by_cat.R
+++ R/num_dist_by_cat.R
@@ -1,5 +1,5 @@
-library( tidyverse)
-library( cowplot)
+#library( tidyverse)
+#library( cowplot)
 
 #' Create a histogram paired with a box plot.
 #'
@@ -22,20 +22,5 @@ library( cowplot)
 #'   title_hist = 'Distribution of X', title_boxplot = 'X Seperated by Group',
 #'   lab_num = 'X', lab_cat = 'Group', num_on_x = True, stat = True)
 num_dist_by_cat <- function( v_num, v_cat, data, title_hist = '', title_boxplot = '', lab_num = '', lab_cat = '', num_on_x = TRUE, stat = TRUE){
-  hist <- data |>
-    ggplot() +
-    aes( x = {{v_num}}) +
-    geom_histogram() +
-    labs( title = title_hist, x = lab_num, y = 'Count')
-
-  boxplot <- data |>
-    ggplot() +
-    aes( x = {{v_cat}}, y = {{v_num}}) +
-    geom_violin() +
-    geom_boxplot( width = 0.1) +
-    labs( title = title_boxplot, x = lab_cat, y = lab_num)
-
-  print( head( select( data, {{ v_cat}})))
-  plot_grid( hist, boxplot, ncol = 2)
 }
 
