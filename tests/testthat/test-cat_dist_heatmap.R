8d549f2e7074fd54faca97d6e5ae6da2eb7cd549
Morris Chan <morrismanfung@gmail.com>
2023-02-03 11:10:50 -0800

    Adding new case to boost coverage.

--- tests/testthat/test-cat_dist_heatmap.R
+++ tests/testthat/test-cat_dist_heatmap.R
@@ -5,13 +5,13 @@ test_that( 'An error should be raised when an empty data frame is passed.', {
   expect_error(cat_dist_heatmap(cat_1 = a,  cat_2 = b, data = empty))
 })
 
-df = mtcars
+df <- mtcars
 test_that( 'An error should be raised when neither heatmap or barchart is selected.', {
   expect_error(
     cat_dist_heatmap(
       cat_1 = vs, cat_2 = am, data = data,
       title = 'Engine vs Transmission',
-      lab_1 = 'Engine', lab_2 = 'Transmission', 
+      lab_1 = 'Engine', lab_2 = 'Transmission',
       heatmap = FALSE, barchart = FALSE
     )
   )
@@ -22,8 +22,20 @@ test_that( 'An error should be raised when neither heatmap or barchart is select
     cat_dist_heatmap(
       cat_1 = vs, cat_2 = qsec, data = data,
       title = 'Engine vs 1/4 mile time',
-      lab_1 = 'Engine', lab_2 = '1/4 mile time', 
+      lab_1 = 'Engine', lab_2 = '1/4 mile time',
       heatmap = TRUE, barchart = FALSE
     )
   )
+})
+
+df <- data.frame( class1 = as.factor( c( 1, 1, 0)),
+              class2 = as.factor( c( 2, 3, 2))
+)
+result <- cat_dist_heatmap(
+  cat_1 = 'class1', cat_2 = 'class2', data= df,
+  title = '', heatmap = TRUE, barchart = TRUE
+)
+test_that( 'A ggplot object is returned', {
+  expect_equal( 'list', typeof( result))
+  expect_equal( class( result)[[1]], 'gg')
 })
